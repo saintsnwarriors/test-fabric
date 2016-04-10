b5b41bba55cd95c05ccd07d68e00514edfaef8c9
saintsnwarriors <banalasanthosh@gmail.com>
2016-04-10 17:15:48 -0400

    testing fabric with git locally

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import local
+
+def test():
+  local("./manage.py")
+
+def commit():
+  local("git add. && git commit -m 'testing fabric with git locally'")
+
+def push():
+  local("git push origin master")
